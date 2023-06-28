953e91e8ce6cb14b6a72666f87b036b234f56c9f
LarsG <lars.gunnar@jobloop.no>
2023-06-28 22:55:36 +0000

    add comments

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,18 +1,20 @@
+# Tag information
 serviceName="k3d-test-homepage-service"
 mode="dev"
 
-# Generate a tag based on the date
+# Generate a tag based on the date and exporting it as an environment variable
 export IMAGE="$serviceName":"$mode"-"$(date +%s)"
-# Setting a temp environment variable that will be accessible in the call to kubectl
 
-
+# Build the frontend
 echo "Building service: $IMAGE"
 docker build -t "$IMAGE" ./apps/homepage
 
+# k3d needs to import the container image
 k3d image import "$IMAGE"
 
 echo "Starting up the services inside the cluster"
+# Read the spec file and use envsubst to replace any variables with env variables then apply
 cat k3s-specification/homepage-service.yaml | envsubst | kubectl apply -f -
 
-# Port forwarding is a simple solution for exposing the application to the localhost
+# Expose the port to the localhost
 kubectl port-forward svc/homepage 8000:80
